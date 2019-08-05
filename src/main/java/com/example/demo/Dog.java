cfe4d56643fa8e39c81c0b36117c1c572145f954
fdmckie <fdmckie@gmail.com>
2019-08-05 18:54:13 -0400

    Exercise 3.01 Dog Saving Data To The Database

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    @Min(1)
+    private int age;
+
+    //@NotNull
+    //@Min(1)
+    //private long id;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
